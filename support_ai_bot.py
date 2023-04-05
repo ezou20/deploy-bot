fb53256c66b93c7854a670254e749c3d286e4caa
Ethan <zouethan20@gmail.com>
2023-04-05 15:45:40 -0400

    working version with memory, and doesn't go crazy responding to everything

--- support_ai_bot.py
+++ support_ai_bot.py
@@ -9,97 +9,125 @@ from langchain.agents import initialize_agent
 
 from llama_index import GPTSimpleVectorIndex
 
+# Global variables
 SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 SLACK_APP_TOKEN = os.environ['SLACK_APP_TOKEN']
 
+# this is our bot's ID
+slack_bot_id = 'U051QHPFKKP'
+# magic emoji that indicates the bot should respond in a thread
+emoji = 'heart_hands'
 app = App(token=SLACK_BOT_TOKEN)
 
-@app.message("hello")
-def message_hello(message, say):
-    result = agent_chain.run("this is a test")
-    print(result)
-    # say() sends a message to the channel where the event was triggered
-    say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click Me"},
-                    "action_id": "button_click"
-                }
-            }
-        ],
-        text=f"Hey there <@{message['user']}>!"
-    )
+# @app.message("hello")
+# def message_hello(message, say):
+#     result = agent_chain.run("this is a test")
+#     print(result)
+#     # say() sends a message to the channel where the event was triggered
+#     say(
+#         blocks=[
+#             {
+#                 "type": "section",
+#                 "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+#                 "accessory": {
+#                     "type": "button",
+#                     "text": {"type": "plain_text", "text": "Click Me"},
+#                     "action_id": "button_click"
+#                 }
+#             }
+#         ],
+#         text=f"Hey there <@{message['user']}>!"
+#     )
 
-@app.action("button_click")
-def action_button_click(body, ack, say):
-    # Acknowledge the action
-    # we could maybe link to the right doc here
-    ack()
-    say(f"<@{body['user']['id']}> clicked the button")
+# @app.action("button_click")
+# def action_button_click(body, ack, say):
+#     # Acknowledge the action
+#     # we could maybe link to the right doc here
+#     ack()
+#     say(f"<@{body['user']['id']}> clicked the button")
 
-@app.event("message")
-def handle_message_events(body, message, say, logger):
-    logger.info(body)
-    text = body['event']['text']
-    thread = body['event']['ts']
-    result = agent_chain.run(text)
-    print("handling thread response")
-    say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"{result}"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click Me"},
-                    "action_id": "button_click"
-                }
-            }
-        ],
-        text=f"Hey there !", thread_ts=thread
+# Check the messages in a thread to see if the Support Bot was mentioned at all and creates a thread history for the LLM to consume
+# Returns true if the bot was found
+def check_messages_in_thread(client, channel_id, thread_ts):
+    print("Looking at thread history")
+    memory.clear()
+    support_bot_mentioned = False
+    conversation_replies = client.conversations_replies(
+        channel=channel_id,
+        ts=thread_ts
     )
-    logger.info(body)
+    # print("conversation replies: ", parent_conversation_object)
+    for message in conversation_replies['messages']:
+        if slack_bot_id in message['text']:
+            support_bot_mentioned = True
+        if message['user'] == slack_bot_id:
+            memory.chat_memory.add_ai_message(message['text'])
+        else:
+            memory.chat_memory.add_user_message(message['text'])
+
+    return support_bot_mentioned
 
 @app.event("app_mention")
-@app.event("message")
-def handle_app_mention_events(body, message, say, logger):
-    print("handling mention event")
-    text = body['event']['text']
-    thread = body['event']['ts']
-    result = agent_chain.run(text)
-    memory.chat_memory.add_user_message(text)
-    memory.chat_memory.add_ai_message(result)
-    print(result)
-    print("Body: ", body)
-    # print(message)
-    say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": f"Hey there <@{body['event']['user']}>! Here's your answer: {result}"},
-                "accessory": {
-                    "type": "button",
-                    "text": {"type": "plain_text", "text": "Click Me"},
-                    "action_id": "button_click"
-                }
-            }
-        ],
-        text=f"Hey there !", thread_ts=thread
-    )
+def handle_app_mention(body, client, logger):
     logger.info(body)
 
-# @app.event("app_mention")
-# async def handle_mentions(event, client, say):  # async function
-#     api_response = await client.reactions_add(
-#         channel=event["channel"],
-#         timestamp=event["ts"],
-#         name="eyes",
-#     )
-#     await say("What's up?")
+@app.event("message")
+def handle_app_message_events(body, client, message, say, logger):
+    logger.info(body)
+    text = body['event']['text']
+    thread = body['event']['ts']
+    if 'thread_ts' in body['event']:
+        thread = body['event']['thread_ts']
+    channel_id = body['event']['channel']
+
+    print("*************************** Handling New Message ************************************")
+    print("THIS IS THE TEXT: ", text)
+    print("THREAD ID: ", thread)
+    print("CHANNEL ID: ", channel_id)
+    
+    # see if the bot was mentioned in the parent thread -- we only want it to respond in threads where it was mentioned
+    parent_conversation_object = client.conversations_history(
+        channel=channel_id,
+        inclusive=True,
+        oldest=thread,
+        limit=1
+    )
+    print("Parent Object: ", parent_conversation_object)
+    parent_message = parent_conversation_object['messages'][0]
+    
+    # check if the parent message has a reaction that matches the magic emoji
+    emoji_in_message = False
+    if 'reactions' in parent_message:
+        parent_message_reactions = parent_conversation_object['messages'][0]['reactions']
+        for reaction in parent_message_reactions:
+            if reaction['name'] == emoji:
+                emoji_in_message = True
+
+    support_bot_in_thread = check_messages_in_thread(client, channel_id, thread)
+
+    if slack_bot_id in text or (support_bot_in_thread and emoji_in_message):
+        print("Generating AI Response...")
+        ai_response = agent_chain.run(text)
+        response_message = f"{ai_response}" # Hey there <@{body['event']['user']}>! 
+        # print("Memory: ", memory.chat_memory)
+        # print(ai_response)
+        # print("Body: ", body)
+        # print(message)
+        say(
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {"type": "mrkdwn", "text": response_message},
+                    "accessory": {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "Click Me"},
+                        "action_id": "button_click"
+                    }
+                }
+            ],
+            text=response_message,
+            thread_ts=thread
+        )
 
 if __name__ == "__main__":
     # initialize LLM
@@ -109,6 +137,12 @@ if __name__ == "__main__":
             name='retool_docs',
             func=lambda question: str(index.query(question)),
             description="Retool Documentation. Useful for answering questions about Retool. Includes best practices, reference API documnetation, how to guides, and reommendations.",
+            return_direct=True
+        ),
+        Tool(
+            name='intro_bot',
+            func=lambda question: f"I am Retool's (emotional) Support AI bot! Ask me anything! If you want me to respond to all messages in this thread, react with :{emoji}: in the parent message of the thread.",
+            description="This bot is used for introducing what it does. Use this tool for when someone asks about the bot and what it does.",
             return_direct=True
         )
     ]
